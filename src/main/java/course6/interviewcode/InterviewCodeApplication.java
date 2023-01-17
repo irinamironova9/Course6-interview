b44d445f5b346aef0fb2f99b58d6c8c2b487990c
Ирина <mi.irene97@gmail.com>
2023-01-17 19:08:56 +0300

    Initial commit

---
+++ src/main/java/course6/interviewcode/InterviewCodeApplication.java
@@ -0,0 +1,13 @@
+package course6.interviewcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course6/interviewcode/InterviewCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package course6.interviewcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
